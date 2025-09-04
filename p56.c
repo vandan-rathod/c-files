1d0face6fa1825339dc4a4fa213c5a900eda4c76
vandan-rathod <vandanrathod1551@gmail.com>
2025-09-04 22:51:34 +0530

    file added

--- p56.c
+++ p56.c
@@ -1,11 +1,32 @@
 #include<stdio.h>
 
 int main(){
-    int a[10],i,j;
+    int a[10],i,j,minindex,t;
     for ( i = 0; i <=9 ; i++)
     {
         printf("Enter a number=");
         scanf("%d",&a[i]);
+    }
+    for ( i = 0; i <=8 ; i++)
+    {
+        minindex=i;
+        for ( j = i+1; j <=9 ; j++)
+        {
+            if (a[j]<a[minindex])
+            {
+                minindex=j;
+            }
+            
+        }
+        t=a[i];
+        a[i]=a[minindex];
+        a[minindex]=t;
+        
+    }
+    printf("sorted array:\t");
+    for ( i = 0; i < 10; i++)
+    {
+        printf("%d ",a[i]);
     }
     
     
