a296c657074b5e1094d1c835841992a063c6f533
vandan-rathod <vandanrathod1551@gmail.com>
2025-09-04 23:47:42 +0530

    file added

---
+++ p59.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+    int a[10],i,j,temp;
+    for ( i = 0; i < 10; i++)
+    {
+        printf("Enter number=");
+        scanf("%d",&a[i]);
+    }
+    for ( i = 0; i <=8 ; i++)
+    {
+        for ( j = 0; j <=9-i ; j++)
+        {
+            if (a[j]<a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+            
+        }
+        
+    }
+    for ( i = 0; i<10 ; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+    
+    return 0;
+}
