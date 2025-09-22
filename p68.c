f87acc4d07bf654af8374612739feb847c33a1e4
vandan-rathod <vandanrathod1551@gmail.com>
2025-09-22 23:10:47 +0530

    file added

---
+++ p68.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    char a;
+    printf("Enter a character=");
+    scanf("%c",&a);
+    printf("THe character entered is %c and ASCII code is %d",a,a);
+    return 0;
+}
