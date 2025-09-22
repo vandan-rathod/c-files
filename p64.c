fd4af2738cd0193663c711849812f066600e120f
vandan-rathod <vandanrathod1551@gmail.com>
2025-09-22 15:30:09 +0530

    file added

---
+++ p64.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int a[5],t;
+    int *p1,*p2;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter a number=");
+        scanf("%d",&a[i]);
+    }
+    printf("Unchanged array:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    
+    p1=&a[0];
+    p2=&a[4];
+    t=*p1;
+    *p1=*p2;
+    *p2=t;
+    printf("changed array:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    
+    return 0;
+}
