6fc2cf83259a0003cacb937bd1ceefbeea6c9467
vandan-rathod <vandanrathod1551@gmail.com>
2025-12-08 12:04:05 +0530

    file added

--- p54.c
+++ p54.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a[10],max,min,sum;
+int main()
+{
+    int a[10], max, min, sum;
     float avg;
-    for (int i = 0; i <=9; i++)
+    for (int i = 0; i <= 9; i++)
     {
         printf("Enter a number=");
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
-    sum=0;
-    for (int i = 0; i <=9 ; i++)
+    sum = 0;
+    for (int i = 0; i <= 9; i++)
     {
-        sum=sum+a[i];
+        sum = sum + a[i];
     }
-    max=a[0];
-    min=a[0];
-    for (int i = 0; i <=9 ; i++)
+    max = a[0];
+    min = a[0];
+    for (int i = 0; i <= 9; i++)
     {
-        if (max<a[i])
+        if (max < a[i])
         {
-            max=a[i];
+            max = a[i];
         }
-        if (min>a[i])
+        if (min > a[i])
         {
-            min=a[i];
+            min = a[i];
         }
-        
     }
-    avg=(float)sum/10;
-    printf("maximum=%d\n",max);
-    printf("minimum=%d\n",min);
-    printf("sum=%d\n",sum);
-    printf("average=%.2f\n",avg);
-    
+    avg = (float)sum / 10;
+    printf("maximum=%d\n", max);
+    printf("minimum=%d\n", min);
+    printf("sum=%d\n", sum);
+    printf("average=%.2f\n", avg);
+
     return 0;
 }
